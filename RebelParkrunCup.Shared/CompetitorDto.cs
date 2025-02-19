50e1a94c3a5482548fc3ce4f6b34b3c4135a9999
Nicholas Bannayi <nbannayi@hotmail.co.uk>
2025-02-19 00:50:55 +0000

    Validation and tidy-up

--- RebelParkrunCup.Shared/CompetitorDto.cs
+++ RebelParkrunCup.Shared/CompetitorDto.cs
@@ -6,11 +6,11 @@ public class CompetitorDto
     
     public int RunnerId { get; set; }
 
-    public string RunnerFirstName { get; set; }
+    public required string RunnerFirstName { get; set; }
     
-    public string RunnerLastName { get; set; }
+    public required string RunnerLastName { get; set; }
 
-    public string RunnerParkrunId { get; set; }
+    public required string RunnerParkrunId { get; set; }
 
     // Time fields.
     public int BaselineTimeMins { get; set; }
--- RebelParkrunCup.Shared/Location.cs
+++ RebelParkrunCup.Shared/Location.cs
@@ -1,5 +1,5 @@
 public class Location
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string? Name { get; set; }
 }
